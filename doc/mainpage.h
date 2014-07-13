1698947262aaf5559d10001a25f67bc897dcea20
antoine <antoine.maleyrie@gmail.com>
2014-07-13 14:29:19 +0200

    update doc

--- doc/mainpage.h
+++ doc/mainpage.h
@@ -12,12 +12,41 @@
 #define MAIN_PAGE_H
 
 //! \mainpage EasyUsb
+//! \b Introduction :
+//! 
 //! EasyUsb est une mini bibliothèque (actuellement écrit en c et c++) proposent
 //! une manipulation basique mais simple de périphérie usb. Elle s'appuyant sur
-//! la libusb1.0 il vous faudra donc, au préalable l'installer.
-//! \see http://www.libusb.org/
+//! la [libusb1.0](http://libusb.org/). il vous faudra donc, au préalable l'installer.
+//! 
+//! - Auteur : Maleyrie Antoine
+//! - Début du projet : 26 Août 2011
+//! - Version actuel : 0.4
+//! - Site web : http://antoine163.github.io/easyusb/
+//! - Compilateur supporter : GCC
+//! - Licence [MIT](https://github.com/antoine163/easyusb/blob/master/LICENSE).
+//! 
+//! 
+//! \b Installation :
 //!
-//! Les différent implémentation:
+//! Incluez dans votre projet, les fichiers ce trouvant dans le dossier de la
+//! versions (C ou C++) que vous souhaitez utiliser.
+//! Vous devez aussi installer la [libusb1.0](http://libusb.org/).
+//! 
+//! \b Utilisation :
+//!
+//! Un exemple et donner dans la documentation pour chaque versions (C ou C++).
+//! Vous devez aussi linker la libusb1.0 (-lusb-1.0). 
+//! 
+//! \b Documentation :
+//!
+//! Les fichiers ont été conçus de sort à générai de la documentation
+//! automatique grâce au logicielle Doxygen. Vous pouvez générai la
+//! documentation en appellent doxygen avec son fichier de configuration se trouvent
+//! dans le dossier doc. 
+//! Vous pouvez aussi directement consulter les pages de la documentation disponible
+//! [ici](http://antoine163.github.io/easyusb/doc/html/index.html).
+//!
+//! \b Implémentations :
 //! - \ref ipcpp
 //! - \ref ipc
 //!
